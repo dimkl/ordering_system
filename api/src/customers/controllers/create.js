c994bda2cdb53c04583b0e2078828e7a65dd3ab2
Dimitris Klouvas <dimitris.klouvas@gmail.com>
2022-04-11 02:34:25 +0300

    feat: customers create endpoint [api]

--- api/src/customers/controllers/create.js
+++ api/src/customers/controllers/create.js
@@ -1,3 +1,22 @@
+const ajv = require('../../shared/ajv');
+
+const schema = require('../schemas/customer.json');
+const Customer = require('../models/customer');
+
+ajv.validateSchema(schema);
+const validate = ajv.compile(schema);
+
 module.exports = async (ctx, next) => {
-  ctx.body = { };
+  try {
+    const data = await validate(ctx.request.body);
+
+    const customer = await Customer.query().insert(data);
+    
+    ctx.body = await Customer.query().modify('publicColumns').findById(customer.id);
+  } catch (err) {
+    console.error(err);
+    ctx.body = err.message;
+    ctx.status = 422;
+    return;
+  }
 }

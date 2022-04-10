784a3ea0a6f88408b43b46732be2d68a8f2f57da
Dimitris Klouvas <dimitris.klouvas@gmail.com>
2022-04-11 00:42:49 +0300

    feat: added customers table with basic columns [api]

---
+++ api/db/seeds/customers.js
@@ -0,0 +1,25 @@
+const bcrypt = require('bcrypt');
+const SALT_ROUNDS = 10;
+
+async function createPassword(passwordLength = 10) {
+  const password = [...Array(10)].map(() => {
+    // 33 - 126 is the alphanumeric range for latin chars with symbols
+    const randomCharCode = ((Math.random() * 100 + 33) % 126) >> 0;
+    return String.fromCharCode(randomCharCode);
+  }).join()
+
+  return bcrypt.hash(password, SALT_ROUNDS);
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> } 
+ */
+exports.seed = async function (knex) {
+  // Deletes ALL existing entries
+  await knex('customers').truncate()
+  await knex('customers').insert([
+    { id: 1, first_name: 'First', last_name: 'Customer', email: 'customer1@example.com', password: await createPassword() },
+    { id: 2, first_name: 'Second', last_name: 'Customer', email: 'customer2@example.com', password: await createPassword() },
+  ]);
+};

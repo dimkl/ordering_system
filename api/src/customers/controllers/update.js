36e3ddd10c94916e5f0ae5ffb46a54ebef7de8ba
Dimitris Klouvas <dimitris.klouvas@gmail.com>
2022-04-11 23:27:59 +0300

    feat: add customer#patch endpoint [api]

--- api/src/customers/controllers/update.js
+++ api/src/customers/controllers/update.js
@@ -1,5 +1,28 @@
-const handler = async (ctx, next) => {
-  ctx.body = {};
-}
+const ajv = require('../../shared/ajv');
+const { ValidationError } = require("ajv");
+const { UniqueViolationError } = require('objection-db-errors');
 
-module.exports = { handler };
+const schema = require('../schemas/customer.patch.json');
+
+ajv.validateSchema(schema);
+const validate = ajv.compile(schema);
+
+const handler = async (ctx, next) => {
+  try {
+    const data = await validate(ctx.request.body);
+
+    await ctx.customer.$query().patch(data);
+    ctx.body = ctx.customer;
+  } catch (err) {
+    debugger;
+    if (err instanceof ValidationError) {
+      ctx.status = 400;
+      ctx.body = err.errors;
+    } else if (err instanceof UniqueViolationError) {
+      ctx.status = 422;
+      ctx.body = { message: `${err.columns.join(',')} already exists!` };
+    }
+  }
+};
+
+module.exports = { handler, schema };

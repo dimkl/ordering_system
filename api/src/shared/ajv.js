2c413f48ea41c4d5dd37ccacfda0417c4139efea
Dimitris Klouvas <dimitris.klouvas@gmail.com>
2022-04-11 23:26:55 +0300

    feat: add http_method & http_path in customer schema for discovery [api]

--- api/src/shared/ajv.js
+++ api/src/shared/ajv.js
@@ -1,7 +1,9 @@
 const Ajv = require("ajv");
 const addFormats = require('ajv-formats');
+const addKeywords = require('./addKeywords');
 
 const ajv = new Ajv({ allErrors: true });
 addFormats(ajv);
+addKeywords(ajv);
 
 module.exports = ajv;

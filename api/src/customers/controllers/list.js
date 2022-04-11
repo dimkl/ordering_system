b039ea73ab08a9f91e91c7be38bc88ab44cd15d6
Dimitris Klouvas <dimitris.klouvas@gmail.com>
2022-04-11 23:27:32 +0300

    feat: implement customer#get endpoint [api]

--- api/src/customers/controllers/list.js
+++ api/src/customers/controllers/list.js
@@ -1,6 +1,10 @@
 const Customer = require('../models/customer');
 
 const handler = async (ctx, next) => {
+  if (ctx.customer){
+    ctx.body = ctx.customer;  
+    return next();
+  }
   ctx.body = await Customer.query().modify('publicColumns');
 };
 

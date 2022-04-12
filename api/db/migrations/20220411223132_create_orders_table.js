8665fef3d6bb495e92188a512ea8773cc6b2b584
Dimitris Klouvas <dimitris.klouvas@gmail.com>
2022-04-13 00:03:21 +0300

    fix: update orders.state column to 50 chars and set default in schema [api]

--- api/db/migrations/20220411223132_create_orders_table.js
+++ api/db/migrations/20220411223132_create_orders_table.js
@@ -7,7 +7,7 @@
     .createTable('orders', function (table) {
       table.increments('id');
       table.integer('customer_id').unsigned();
-      table.string('state', 255).notNullable().defaultTo('draft');
+      table.string('state', 50).notNullable().defaultTo('draft');
       table.timestamps(true, true);
 
       table.foreign('customer_id').references('customers.id')

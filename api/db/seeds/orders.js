36f76b233cf8b695449a47f5cc041cac900556e8
Dimitris Klouvas <dimitris.klouvas@gmail.com>
2022-04-13 00:08:00 +0300

    fix: update seeds [api]

--- api/db/seeds/orders.js
+++ api/db/seeds/orders.js
@@ -3,8 +3,7 @@
  * @returns { Promise<void> } 
  */
 exports.seed = async function (knex) {
-  // Deletes ALL existing entries
-  await knex('orders').truncate();
+  await knex.raw('truncate orders, order_items cascade');
   await knex('orders').insert([
     { customer_id: 1, state: 'draft' },
   ]);
--- api/db/seeds/products.js
+++ api/db/seeds/products.js
@@ -3,8 +3,7 @@
  * @returns { Promise<void> } 
  */
 exports.seed = async function (knex) {
-  // Deletes ALL existing entries
-  await knex('products').truncate();
+  await knex.raw('truncate products, orders, order_items cascade');
   await knex('products').insert([
     { title: "Product", sku: "product-code-1", description: "Product description" }
   ]);

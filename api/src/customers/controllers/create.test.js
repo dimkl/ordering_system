e072d5205c75461f4e07167e4cfe79a04b4cc473
Dimitris Klouvas <dimitris.klouvas@gmail.com>
2022-04-12 23:05:34 +0300

    fix: update tests to handle truncate with customer_id foreign key [api]

--- api/src/customers/controllers/create.test.js
+++ api/src/customers/controllers/create.test.js
@@ -8,7 +8,7 @@ describe("POST /customers", () => {
   let request = {};
 
   beforeAll(() => { request = supertest(app) });
-  beforeEach(() => Customer.query().truncate());
+  beforeEach(() => Customer.knex().raw('truncate customers, orders cascade;'));
   afterAll(() => {
     app.close();
     Customer.knex().destroy();
@@ -25,7 +25,9 @@ describe("POST /customers", () => {
       .set("Accept", "application/json");
 
     expect(response.status).toBe(200);
-    expect(response.body).toMatchSnapshot();
+    expect(response.body).toMatchSnapshot({
+      id: expect.anything()
+    });
   });
 
   it("throws validation error for required properties", async () => {

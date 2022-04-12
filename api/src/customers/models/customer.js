3c199e7da98930f1a1a6e0f88d844475a846e340
Dimitris Klouvas <dimitris.klouvas@gmail.com>
2022-04-13 02:25:52 +0300

    feat: add customer password hashing support [api]

--- api/src/customers/models/customer.js
+++ api/src/customers/models/customer.js
@@ -1,11 +1,11 @@
 const BaseModel = require('../../shared/baseModel');
-
+const Password = require('objection-password')();
 const schema = require('../schemas/customer.json');
 
 const SENSITIVE_COLUMNS = ['password'];
 const PUBLIC_COLUMNS = Object.keys(schema.properties).filter(k => !SENSITIVE_COLUMNS.includes(k));
 
-class Customer extends BaseModel {
+class Customer extends Password(BaseModel) {
   static get tableName() {
     return 'customers';
   }

d1447ff33c09fddde53fdbaccb997f3c3349b28f
Dimitris Klouvas <dimitris.klouvas@gmail.com>
2022-04-20 01:20:30 +0300

    fix: users seeds & update user schema [api]

---
+++ api/db/seeds/5.users.js
@@ -0,0 +1,24 @@
+const bcrypt = require('bcrypt');
+const SALT_ROUNDS = 10;
+const uuid = require('uuid');
+
+async function createPassword(passwordLength = 10) {
+  const password = [...Array(passwordLength)].map(() => {
+    // 33 - 126 is the alphanumeric range for latin chars with symbols
+    const randomCharCode = ((Math.random() * 100 + 33) % 126) >> 0;
+    return String.fromCharCode(randomCharCode);
+  }).join('')
+
+  return bcrypt.hash(password, SALT_ROUNDS);
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> } 
+ */
+exports.seed = async function (knex) {
+  await knex('users').insert([
+    { first_name: 'First', last_name: 'Customer', email: 'customer1@example.com', password: await createPassword(), uuid: uuid.v4()},
+    { first_name: 'Second', last_name: 'Customer', email: 'customer2@example.com', password: await createPassword(), uuid: uuid.v4() },
+  ]);
+};

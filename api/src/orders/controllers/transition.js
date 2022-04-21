c285a7984993032f67a9441b9779e023096a7f4b
Dimitris Klouvas <dimitris.klouvas@gmail.com>
2022-04-21 23:25:34 +0300

    feat: order#transition and orderItem#transition endpoints [api]

--- api/src/orders/controllers/transition.js
+++ api/src/orders/controllers/transition.js
@@ -1,5 +1,26 @@
-const handler = async (ctx, next) => {
-  ctx.body = {};
+const ajv = require('../../shared/ajv');
+
+const Order = require('../models/order');
+
+const schema = require('../schemas/order.transition.json');
+
+ajv.addSchema(schema);
+ajv.validateSchema(schema);
+
+async function handler(ctx, next) {
+  const validate = ajv.compile(schema);
+
+  const data = await validate({ state: ctx.params.state });
+
+  if (Object.keys(data).length > 0) {
+    await ctx.order.$query().patch(data);
+  }
+
+  if (ctx.order.state === 'placed') {
+    await ctx.order.$relatedQuery('order_items').patch({ state: 'requested' });
+  }
+
+  ctx.body = await Order.findWithOrderItemsAndProducts(ctx.order.id);;
 }
 
-module.exports = { handler };
+module.exports = { handler, schema };

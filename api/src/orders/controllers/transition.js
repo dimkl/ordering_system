8d6e9695d1c3979de8449c029cf870fd0ed4fd8b
Dimitris Klouvas <dimitris.klouvas@gmail.com>
2022-04-22 23:16:52 +0300

    fix: replace state with action in order & orderItem transitions endpoints [api]

--- api/src/orders/controllers/transition.js
+++ api/src/orders/controllers/transition.js
@@ -1,6 +1,7 @@
 const ajv = require('../../shared/ajv');
 
 const Order = require('../models/order');
+const OrderTransition = require('../services/orderTransition');
 
 const schema = require('../schemas/order.transition.json');
 
@@ -10,17 +11,12 @@ ajv.validateSchema(schema);
 async function handler(ctx, next) {
   const validate = ajv.compile(schema);
 
-  const data = await validate({ state: ctx.params.state });
+  const data = await validate({ action: ctx.params.action });
 
-  if (Object.keys(data).length > 0) {
-    await ctx.order.$query().patch(data);
-  }
+  const order = await Order.findWithOrderItemsAndProducts(ctx.order.id);
+  await OrderTransition.process(order, data.action);
 
-  if (ctx.order.state === 'placed') {
-    await ctx.order.$relatedQuery('order_items').patch({ state: 'requested' });
-  }
-
-  ctx.body = await Order.findWithOrderItemsAndProducts(ctx.order.id);;
+  ctx.body = order;
 }
 
 module.exports = { handler, schema };
--- api/src/orders/controllers/transitionOrderItem.js
+++ api/src/orders/controllers/transitionOrderItem.js
@@ -1,6 +1,7 @@
 const ajv = require('../../shared/ajv');
 
 const Order = require('../models/order');
+const OrderItemTransition = require('../services/orderItemTransition');
 
 const schema = require('../schemas/orderItem.transition.json');
 
@@ -10,21 +11,12 @@ ajv.validateSchema(schema);
 async function handler(ctx, next) {
   const validate = ajv.compile(schema);
 
-  const data = await validate({ state: ctx.params.state });
-
-  if (Object.keys(data).length > 0) {
-    await ctx.orderItem.$query().patch(data);
-  }
+  const data = await validate({ action: ctx.params.action });
 
   const order = await Order.findWithOrderItemsAndProducts(ctx.orderItem.order_id);
+  const orderItem = order.order_items.filter(oi => oi.id === ctx.orderItem.id)[0];
+  await OrderItemTransition.process(orderItem, order, data.action);
 
-  const allOrderItemsDelivered = order.order_items.every(oi => oi.state === 'delivered');
-  if (order.state === 'placed' && ctx.orderItem.state === 'prepared') {
-    await order.$query().patch({ state: 'processing' });
-  } else if (order.state === 'processing' && allOrderItemsDelivered) {
-    await order.$query().patch({ state: 'delivered' });
-  }
-  
   ctx.body = order;
 }
 

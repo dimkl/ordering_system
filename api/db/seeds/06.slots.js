3dc95ce43727dda2f2fd819871a55402bba42363
Dimitris Klouvas <dimitris.klouvas@gmail.com>
2022-04-24 01:23:12 +0300

    feat: add slots#capacity in model & seeds & db [api]

--- api/db/seeds/06.slots.js
+++ api/db/seeds/06.slots.js
@@ -2,6 +2,6 @@ const uuid = require('uuid');
 
 exports.seed = async function (knex) {
   await knex('slots').insert([
-    { section_id: 1, user_id: 1, sku: 'table-1', uuid: uuid.v4(), active: true }
+    { section_id: 1, user_id: 1, sku: 'table-1', uuid: uuid.v4(), active: true, capacity: 5 }
   ]);
 };

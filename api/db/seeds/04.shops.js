a1babd0bfdf4d7564467eb93286149207b776301
Dimitris Klouvas <dimitris.klouvas@gmail.com>
2022-04-23 22:29:40 +0300

    fix: shop and time_slots seeds [api]

--- api/db/seeds/04.shops.js
+++ api/db/seeds/04.shops.js
@@ -2,6 +2,6 @@ const uuid = require('uuid');
 
 exports.seed = async function (knex) {
   await knex('shops').insert([
-    { manager_id: 1, name: 'Shop', uuid: uuid.v4(), opening_time: '08:00:00', closing_time: '00:00:00', opening_days: '{0,1,2,3,4,5}' }
+    { manager_id: 1, name: 'Shop', uuid: uuid.v4(), opening_time: '08:00', closing_time: '00:00', opening_days: '{0,1,2,3,4,5}' }
   ]);
 };
--- api/db/seeds/07.time_slots.js
+++ api/db/seeds/07.time_slots.js
@@ -1,9 +1,9 @@
 const uuid = require('uuid');
 
 exports.seed = async function (knex) {
-  const now = Date.now() / 1000;
+  const now = Date.now();
 
   await knex('time_slots').insert([
-    { slot_id: 1, customer_id: 1, uuid: uuid.v4(), started_at: new Date(now), ended_at: new Date(now + 3600) }
+    { slot_id: 1, customer_id: 1, uuid: uuid.v4(), started_at: new Date(now), ended_at: new Date(now + 3600000) }
   ]);
 };

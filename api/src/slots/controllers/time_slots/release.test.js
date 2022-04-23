b3207eae45e6e32bcd303025a6868296bab19987
Dimitris Klouvas <dimitris.klouvas@gmail.com>
2022-04-24 02:56:02 +0300

    refactor: rename time_slots#reserve & time_slots#release endpoints [api]

--- api/src/slots/controllers/time_slots/release.test.js
+++ api/src/slots/controllers/time_slots/release.test.js
@@ -4,7 +4,7 @@
  */
 const uuid = require('uuid');
 
-describe("DELETE /time_slots/:time_slot_id", () => {
+describe("DELETE /time_slots/release/:time_slot_id", () => {
   let knex;
   beforeAll(() => knex = require('../../../shared/setupModels')());
   beforeEach(() => knex.raw('truncate orders, order_items, customers, users, products, time_slots, slots cascade;'));
@@ -13,7 +13,7 @@ describe("DELETE /time_slots/:time_slot_id", () => {
   it("deletes time_slot and returns 204", async () => {
     const timeSlot = await DataFactory.createTimeSlot();
 
-    const response = await request.delete(`/time_slots/${timeSlot.id}`)
+    const response = await request.delete(`/time_slots/release/${timeSlot.id}`)
       .set("Accept", "application/json");
 
     expect(response.status).toBe(204);
@@ -22,14 +22,14 @@ describe("DELETE /time_slots/:time_slot_id", () => {
   it("deletes time_slot using uuid and returns 204", async () => {
     const timeSlot = await DataFactory.createTimeSlot();
 
-    const response = await request.delete(`/time_slots/${timeSlot.uuid}`)
+    const response = await request.delete(`/time_slots/release/${timeSlot.uuid}`)
       .set("Accept", "application/json");
 
     expect(response.status).toBe(204);
   });
 
   it("throws 404 when time_slot does not exist", async () => {
-    const response = await request.delete(`/time_slots/${uuid.v4()}`)
+    const response = await request.delete(`/time_slots/release/${uuid.v4()}`)
       .set("Accept", "application/json");
 
     expect(response.status).toBe(404);

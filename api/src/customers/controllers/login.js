649060b9c523c0e431836a1d84a4ea86132e5426
Dimitris Klouvas <dimitris.klouvas@gmail.com>
2022-04-24 04:41:55 +0300

    feat: customers#login endpoint [api]

--- api/src/customers/controllers/login.js
+++ api/src/customers/controllers/login.js
@@ -1,5 +1,26 @@
-const handler = async (ctx, next) => {
-  ctx.body = {};
+const ajv = require('../../shared/ajv');
+const { BusinessError } = require('../../shared/errors');
+
+const schema = require('../schemas/customer.login.json');
+const Customer = require('../models/customer');
+
+const createJwt = require('../helpers/createJwt');
+
+ajv.addSchema(schema);
+ajv.validateSchema(schema);
+
+async function handler(ctx, next) {
+  const validate = ajv.compile(schema);
+  const { email, password } = await validate(ctx.request.body);
+
+  const customer = await Customer.query().where({ email }).first();
+  const isPasswordValid = await customer.verifyPassword(password);
+
+  if (!isPasswordValid) {
+    throw new BusinessError('Password or email is not valid. Please try again!');
+  }
+
+  ctx.body = await createJwt(customer.id, customer.uuid);
 }
 
-module.exports = handler ;
+module.exports = handler;

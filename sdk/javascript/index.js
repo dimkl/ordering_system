81398f0d6113ef4d82c38febd08dcbb45cf966ca
Dimitris Klouvas <dimitris.klouvas@gmail.com>
2022-04-24 19:30:44 +0300

    feat: add ajv validation and change order of `body` & `params` in request method [sdk]

--- sdk/javascript/index.js
+++ sdk/javascript/index.js
@@ -1,4 +1,34 @@
 const fetch = require('isomorphic-fetch');
+const Ajv = require("ajv");
+const addFormats = require('ajv-formats');
+
+// see: api/src/shared/addKeywords.js
+function addKeywords(ajv) {
+  ajv.addKeyword({
+    keyword: "http_method",
+    validate: () => true,
+    errors: false,
+  });
+  ajv.addKeyword({
+    keyword: "http_path",
+    validate: () => true,
+    errors: false,
+  });
+}
+
+function createApi(schemas) {
+  return schemas.reduce((api, schema) => {
+    if (schema["http_path"] && schema["http_method"]) {
+      // convert method to camel case
+      const apiMethod = schema["$id"]
+        .replace('/schemas/', '')
+        .replace(/(\.(\w))/, (_1, _2, a) => a.toUpperCase());
+      api[apiMethod] = schema["$id"];
+    }
+
+    return api;
+  }, {});
+}
 
 async function request(endpoint, jwt, body, options = {}) {
   const fetchOptions = {
@@ -8,7 +38,7 @@ async function request(endpoint, jwt, body, options = {}) {
   };
 
   if (jwt) {
-    fetchOptions.headers['Authenication'] = `Bearer ${jwt}`;
+    fetchOptions.headers['Authentication'] = `Bearer ${jwt}`;
   }
 
   if (body) {
@@ -23,40 +53,38 @@ async function request(endpoint, jwt, body, options = {}) {
     return jsonResponse;
   });
 }
-
 class SDK {
-  constructor(api, apiUrl, jwt) {
+  constructor(schemas, apiUrl, jwt) {
     this.apiUrl = apiUrl;
     this.jwt = jwt;
 
-    Object.entries(api).map(([methodName, { path, method }]) => {
-      this[methodName] = this.handler.bind(this, path, method);
+    this.ajv = new Ajv({ allErrors: true, schemas });
+    addFormats(this.ajv);
+    addKeywords(this.ajv);
+
+    const api = createApi(schemas);
+    Object.entries(api).map(([methodName, schemaId]) => {
+      this[methodName] = this.handler.bind(this, schemaId);
     });
   }
 
-  async handler(path, method, params, body) {
+  async handler(schemaId, body, params = {}) {
+    const schema = this.ajv.getSchema(schemaId).schema;
+    const { http_path: path, http_method: method } = schema;
+
+    const validate = this.ajv.compile(schema);
+    const data = await validate(body);
+
     const requestPath = path.replace(/:([\w]+)/, (_, a) => params[a]);
     const requestUrl = (new URL(requestPath, this.apiUrl)).href;
-
-    return request(requestUrl, this.jwt, body, { method });
+    return request(requestUrl, this.jwt, data, { method });
   }
-}
-
-function createApi(schemas) {
-  return schemas.reduce((api, schema) => {
-    let apiMethod = schema["$id"].replace('/schemas/', '');
-    // convert method to camel case
-    apiMethod = apiMethod.replace(/(\.(\w))/, (_1, _2, a) => a.toUpperCase());
-    api[apiMethod] = { path: schema["http_path"], method: schema["http_method"] };
-
-    return api;
-  }, {});
 }
 
 async function SDKFactory({ apiUrl, path = '/discovery', jwt }) {
   const { schemas } = await request(new URL(path, apiUrl).href);
 
-  return new SDK(createApi(schemas), apiUrl, jwt);
+  return new SDK(schemas, apiUrl, jwt);
 }
 
 module.exports = SDKFactory;

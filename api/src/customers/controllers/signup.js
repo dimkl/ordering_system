61698a6bf60b0946c68ae9857b23fe8c8366611b
Dimitris Klouvas <dimitris.klouvas@gmail.com>
2022-04-24 04:19:37 +0300

    feat: expose schemas and publicKey in /discovery & impl signup [api]

--- api/src/customers/controllers/signup.js
+++ api/src/customers/controllers/signup.js
@@ -1,5 +1,30 @@
-const handler = async (ctx, next) => {
-  ctx.body = {};
+const fs = require('fs');
+const jsonwebtoken = require('jsonwebtoken');
+const ajv = require('../../shared/ajv');
+
+const schema = require('../schemas/customer.signup.json');
+const Customer = require('../models/customer');
+
+ajv.addSchema(schema);
+ajv.validateSchema(schema);
+
+const JWT_ALGORITHM = process.env.JWT_ALGORITHM || 'RS256';
+const JWT_EXPIRATION = process.env.JWT_EXPIRATION || '1h';
+const JWT_PRIVATE_KEY = process.env.JWT_PRIVATE_KEY || fs.readFileSync(__dirname + '/../../../config/private.key');
+
+async function handler(ctx, next) {
+  const validate = ajv.compile(schema);
+  const { repeat_password, ...data } = await validate(ctx.request.body);
+
+  const { id: customerId, uuid } = await Customer.query().modify('publicInsertColumns').insert(data);
+
+  const customer = await Customer.query().modify('tokenColumns').findById(customerId);;
+  const payload = { role: 'customer', sub: uuid, ...customer };
+
+  ctx.body = await jsonwebtoken.sign(payload, JWT_PRIVATE_KEY, {
+    expiresIn: JWT_EXPIRATION,
+    algorithm: JWT_ALGORITHM
+  });
 }
 
-module.exports = handler ;
+module.exports = handler;
--- api/src/router.js
+++ api/src/router.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const Router = require('koa-router');
 const ajv = require('./shared/ajv');
 
@@ -8,10 +9,13 @@ router.use(require('./orders/router').routes());
 router.use(require('./products/router').routes());
 router.use(require('./slots/router').routes());
 
+const JWT_PUBLIC_KEY = process.env.JWT_PUBLIC_KEY || fs.readFileSync(__dirname + '/../config/public.pem');
 router.get('/discovery', (ctx) => {
-  ctx.body = Object.keys(ajv.schemas)
+  const schemas = Object.keys(ajv.schemas)
     .filter(s => s.startsWith('/schemas/'))
     .map(s => ajv.getSchema(s).schema);
+
+  ctx.body = { schemas, publicKey: JWT_PUBLIC_KEY };
 });
 
 module.exports = router;

266f40bd1d8fd263a1ee434fbb53b17aadd728b1
Dimitris Klouvas <dimitris.klouvas@gmail.com>
2022-04-24 23:02:21 +0300

    refactor: available slots endpoint [api]

--- api/src/slots/controllers/slots/available.test.js
+++ api/src/slots/controllers/slots/available.test.js
@@ -8,7 +8,7 @@ describe("GET /slots/:shop_id/available/:slot_id?", () => {
   beforeEach(() => knex.raw('truncate orders, order_items, customers, users, products, time_slots, slots cascade;'));
   afterAll(() => knex.destroy());
 
-  it("returns all slots", async () => {
+  it("returns all available slots", async () => {
     const { section } = await DataFactory.createSlot();
 
     const response = await request.get(`/slots/${section.shop_id}/available`)
@@ -26,41 +26,7 @@ describe("GET /slots/:shop_id/available/:slot_id?", () => {
     });
   });
 
-  it("returns specified slot", async () => {
-    const slot = await DataFactory.createSlot();
-
-    const response = await request.get(`/slots/${slot.section.shop_id}/available/${slot.id}`)
-      .set("Accept", "application/json");
-
-    expect(response.status).toBe(200);
-    expect(response.body).toMatchSnapshot({
-      id: expect.any(Number),
-      section_id: expect.any(String),
-      user_id: expect.any(String),
-      uuid: expect.any(String),
-      created_at: expect.any(String),
-      updated_at: expect.any(String),
-    });
-  });
-
-  it("returns specified slot using uuid", async () => {
-    const slot = await DataFactory.createSlot();
-
-    const response = await request.get(`/slots/${slot.section.shop_id}/available/${slot.id}`)
-      .set("Accept", "application/json");
-
-    expect(response.status).toBe(200);
-    expect(response.body).toMatchSnapshot({
-      id: expect.any(Number),
-      section_id: expect.any(String),
-      user_id: expect.any(String),
-      uuid: expect.any(String),
-      created_at: expect.any(String),
-      updated_at: expect.any(String),
-    });
-  });
-
-  it("returns empty list of slots when there are no slots", async () => {
+  it("returns empty list of slots when there are no available slots", async () => {
     const slot = await DataFactory.createSlot({ active: false });
 
     const response = await request.get(`/slots/${slot.section.shop_id}/available`)
@@ -68,6 +34,40 @@ describe("GET /slots/:shop_id/available/:slot_id?", () => {
 
     expect(response.status).toBe(200);
     expect(response.body).toEqual([]);
+  });
+
+  it("returns available specified slot", async () => {
+    const slot = await DataFactory.createSlot();
+
+    const response = await request.get(`/slots/${slot.section.shop_id}/available/${slot.id}`)
+      .set("Accept", "application/json");
+
+    expect(response.status).toBe(200);
+    expect(response.body).toMatchSnapshot({
+      id: expect.any(Number),
+      section_id: expect.any(String),
+      user_id: expect.any(String),
+      uuid: expect.any(String),
+      created_at: expect.any(String),
+      updated_at: expect.any(String),
+    });
+  });
+
+  it("returns available specified slot using uuid", async () => {
+    const slot = await DataFactory.createSlot();
+
+    const response = await request.get(`/slots/${slot.section.shop_id}/available/${slot.id}`)
+      .set("Accept", "application/json");
+
+    expect(response.status).toBe(200);
+    expect(response.body).toMatchSnapshot({
+      id: expect.any(Number),
+      section_id: expect.any(String),
+      user_id: expect.any(String),
+      uuid: expect.any(String),
+      created_at: expect.any(String),
+      updated_at: expect.any(String),
+    });
   });
 
   it("throws 404 when specified slot does not exist", async () => {

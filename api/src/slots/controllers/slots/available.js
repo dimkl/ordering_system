557dc45abfde620106cb634b2bbd4c90c3d88cc5
Dimitris Klouvas <dimitris.klouvas@gmail.com>
2022-04-24 20:57:01 +0300

    fix: replace slots#list with slots#available using shop_id [api]

---
+++ api/src/slots/controllers/slots/available.js
@@ -0,0 +1,23 @@
+const Slot = require('../../models/slot');
+
+const handler = async (ctx, next) => {
+  if (ctx.slot) {
+    ctx.body = ctx.slot;
+    return next();
+  }
+  const startedAt = ctx.query.started_at || ctx.shop.openingDate(new Date()).toISOString();
+  const defaultEndedAt = ctx.shop.closingDate(new Date(startedAt)).toISOString();
+  const endedAt = ctx.query.ended_at || defaultEndedAt;
+  debugger;
+  console.log('sql: ', Slot.query()
+    .modify(['publicColumns', 'active'])
+    .modify('available', ctx.shop.id, startedAt, endedAt)
+    .toKnexQuery()
+    .toString()
+  );
+  ctx.body = await Slot.query()
+    .modify(['publicColumns', 'active'])
+    .modify('available', ctx.shop.id, startedAt, endedAt);
+};
+
+module.exports = handler;

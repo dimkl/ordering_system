fd89269d7ed02d779c71f4f25a37e584afa2436e
Dimitris Klouvas <dimitris.klouvas@gmail.com>
2022-06-23 01:09:26 +0300

    feat: add ingredient table & model & schema & seed [api]
    Refactor products/controllers to have separated folder for products
    and ingredients handlers.

--- api/src/products/controllers/products/list.js
+++ api/src/products/controllers/products/list.js
@@ -1,4 +1,4 @@
-const Product = require('../models/product');
+const Product = require('../../models/product');
 
 const handler = async (ctx, next) => {
   if (ctx.product){
--- api/src/products/router.js
+++ api/src/products/router.js
@@ -3,19 +3,28 @@ const Router = require('koa-router');
 const verifyToken = require('../shared/middlewares/verifyToken');
 const authorize = require('../shared/middlewares/authorize');
 
-const ListController = require('./controllers/list');
-const CreateController = require('./controllers/create');
+const ProductListController = require('./controllers/products/list');
+const ProductCreateController = require('./controllers/products/create');
+
+const IngredientListController = require('./controllers/ingredients/list');
+const IngredientCreateController = require('./controllers/ingredients/create');
 
 const loadProduct = require('./helpers/loadProduct');
+const loadIngredient = require('./helpers/loadIngredient');
 
 const router = new Router();
 
 // setup params
-router.param('product_id', loadProduct);
+router.param('product_id', loadProduct)
+      .param('ingredient_id', loadIngredient);
 router.use(verifyToken());
 
-router.get('/products', authorize('urn:products:r'), ListController);
-router.get('/products/:product_id', authorize('urn:products:r'), ListController);
-router.post('/products', authorize('urn:products:c'), CreateController);
+router.get('/products', authorize('urn:products:r'), ProductListController);
+router.get('/products/:product_id', authorize('urn:products:r'), ProductListController);
+router.post('/products', authorize('urn:products:c'), ProductCreateController);
+
+router.get('/ingredients', authorize('urn:ingredients:r'), IngredientListController);
+router.get('/ingredients/:ingredient_id', authorize('urn:ingredients:r'), IngredientListController);
+router.post('/ingredients', authorize('urn:ingredients:c'), IngredientCreateController);
 
 module.exports = router;

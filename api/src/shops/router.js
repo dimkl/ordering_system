9a3de218a94cb44ad100c578fe6701841ab126d8
Dimitris Klouvas <dimitris.klouvas@gmail.com>
2022-06-23 02:17:06 +0300

    fix: authorize middleware requiredScopes argument usage [api]

--- api/src/shops/router.js
+++ api/src/shops/router.js
@@ -13,8 +13,8 @@ const router = new Router();
 router.param('shop_id', loadShop);
 router.use(verifyToken());
 
-router.get('/shops/:shop_id/menu', authorize('urn:shops:m'), MenuController);
-router.get('/shops', authorize('urn:shops:r'), ListController);
-router.get('/shops/:shop_id', authorize('urn:products:r'), ListController);
+router.get('/shops/:shop_id/menu', authorize(['urn:shops:m']), MenuController);
+router.get('/shops', authorize(['urn:shops:r']), ListController);
+router.get('/shops/:shop_id', authorize(['urn:products:r']), ListController);
 
 module.exports = router;

122c5e4c3dcb3221c2da19397429b7d4b5731178
Dimitris Klouvas <dimitris.klouvas@gmail.com>
2022-06-23 02:23:11 +0300

    feat: add scopes to jwt defined in customer model [api]

--- api/src/customers/helpers/createJwt.js
+++ api/src/customers/helpers/createJwt.js
@@ -6,23 +6,36 @@ const JWT_ALGORITHM = process.env.JWT_ALGORITHM || 'RS256';
 const JWT_EXPIRATION = process.env.JWT_EXPIRATION || '1h';
 const JWT_PRIVATE_KEY = process.env.JWT_PRIVATE_KEY;
 
-const ROLE = 'customer';
+// function intersection(setA, setB) {
+//   setB = new Set(setB);
+//   return [...new Set(
+//     [...setA].filter(element => setB.has(element))
+//   )];
+// }
 
-async function createJwt(customerUuid) {
+async function createJwt(customerUuid, { role = 'customer' } = {}) {
   const customer = await Customer.query()
     .modify('tokenColumns')
     .throwIfNotFound()
     .where({ uuid: customerUuid })
     .first();
 
-  const payload = { role: ROLE, sub: customerUuid, ...customer };
+  // const allowedScopes = intersection(customer.scopes, scopes);
+  const allowedScopes = customer.scopes.join(' ');
+
+  const payload = {
+    role,
+    scopes: allowedScopes,
+    sub: customerUuid,
+    ...customer
+  };
 
   const access_token = await jsonwebtoken.sign(payload, JWT_PRIVATE_KEY, {
     expiresIn: JWT_EXPIRATION,
     algorithm: JWT_ALGORITHM
   });
 
-  return { role: ROLE, access_token };
+  return { role, access_token, scopes: allowedScopes };
 }
 
 module.exports = createJwt

90d1949f3efcb5566aeac4a6cf0798a9f04d9e3e
Dimitris Klouvas <dimitris.klouvas@gmail.com>
2024-07-12 00:15:27 +0300

    fix(api): Add tests for slot availability

--- api/src/availability/controllers/slots/available.test.ts
+++ api/src/availability/controllers/slots/available.test.ts
@@ -80,6 +80,19 @@ describe("GET /slots/:shop_id/available/:slot_id?", () => {
     });
   });
 
+  it("returns 422 for inactive specified slot", async () => {
+    const slot = await DataFactory.createSlot({ active: false });
+
+    const response = await request
+      .get(`/slots/${slot.section.shop_id}/available/${slot.id}`)
+      .set("Accept", "application/json");
+
+    expect(response.status).toBe(422);
+    expect(response.body).toEqual({
+      message: `Slot ${slot.uid} is not available!`
+    });
+  });
+
   it("throws 404 when specified slot does not exist", async () => {
     const slot = await DataFactory.createSlot();
 
@@ -89,5 +102,82 @@ describe("GET /slots/:shop_id/available/:slot_id?", () => {
       .set("Accept", "application/json");
 
     expect(response.status).toBe(404);
+  });
+
+  it("returns 422 for shop not yet opened", async () => {
+    const slot = await DataFactory.createSlot();
+
+    const startedAt = new Date();
+    startedAt.setUTCHours(14, 55);
+
+    const endedAt = new Date();
+    endedAt.setUTCHours(18);
+
+    const filters = `started_at=${startedAt.toISOString()}&ended_at=${endedAt.toISOString()}`;
+
+    const response = await request
+      .get(`/slots/${slot.section.shop_id}/available?${filters}`)
+      .set("Accept", "application/json");
+
+    expect(response.status).toBe(422);
+    expect(response.body).toEqual({
+      message: `Shop is not open between "${startedAt.toISOString()}" - "${endedAt.toISOString()}"!`
+    });
+  });
+
+  it("returns 422 for closed shop due to passed hour", async () => {
+    const slot = await DataFactory.createSlot({ active: false });
+
+    const startedAt = new Date();
+    startedAt.setUTCHours(20);
+
+    const endedAt = new Date();
+    endedAt.setUTCHours(23, 30);
+
+    const filters = `started_at=${startedAt.toISOString()}&ended_at=${endedAt.toISOString()}`;
+
+    const response = await request
+      .get(`/slots/${slot.section.shop_id}/available?${filters}`)
+      .set("Accept", "application/json");
+
+    expect(response.status).toBe(422);
+    expect(response.body).toEqual({
+      message: `Shop is not open between "${startedAt.toISOString()}" - "${endedAt.toISOString()}"!`
+    });
+  });
+
+  //TODO: implement
+  it.skip("returns 422 for shop holiday", async () => {
+    const slot = await DataFactory.createSlot({ active: false });
+
+    const response = await request
+      .get(`/slots/${slot.section.shop_id}/available`)
+      .set("Accept", "application/json");
+
+    expect(response.status).toBe(422);
+    expect(response.body).toEqual({
+      message: `Slot ${slot.uid} is not available!`
+    });
+  });
+
+  // TODO: implement
+  it.skip("returns all available slots based on date-time range", async () => {
+    const slot1 = await DataFactory.createSlot();
+    const slot2 = await DataFactory.createSlot({}, slot1.section, slot1.user, slot1.shop);
+
+    const response = await request
+      .get(`/slots/${slot1.section.shop_id}/available`)
+      .set("Accept", "application/json");
+
+    expect(response.status).toBe(200);
+    expect(response.body.length).toBe(1);
+    expect(response.body[0]).toMatchSnapshot({
+      id: expect.any(Number),
+      section_id: expect.any(String),
+      user_id: expect.any(String),
+      uuid: expect.any(String),
+      created_at: expect.any(String),
+      updated_at: expect.any(String)
+    });
   });
 });
--- api/src/availability/controllers/slots/available.ts
+++ api/src/availability/controllers/slots/available.ts
@@ -1,12 +1,18 @@
 import type { Context, Next } from "koa";
 
 import { Slot } from "../../models";
+import { BusinessError } from "../../../shared/errors";
 
 export const handler = async (ctx: Context, next: Next) => {
   if (ctx.slot) {
+    if (!ctx.slot.active) {
+      throw new BusinessError(`Slot ${ctx.slot.uid} is not available!`);
+    }
+
     ctx.body = ctx.slot;
     return next();
   }
+
   const startedAt = ctx.query.started_at || ctx.shop.openingDate(new Date()).toISOString();
   const defaultEndedAt = ctx.shop.closingDate(new Date(startedAt)).toISOString();
   const endedAt = ctx.query.ended_at || defaultEndedAt;

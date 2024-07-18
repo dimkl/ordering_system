f2daf8b1b9f5f52557488bef55fca6b62b9d89e8
Dimitris Klouvas <dimitris.klouvas@gmail.com>
2024-07-18 23:19:29 +0300

    chore(api): Replace npm script execution with knex.migrate in jest setup for `npm run test`

--- api/config/jest/setup.ts
+++ api/config/jest/setup.ts
@@ -1,9 +1,7 @@
 import { knex } from "../../src/shared/knex";
 
 export default async () => {
-  // TODO: replace `npm run db:migrate` in package with the line above
-  // Currently there is an issue with esm migration files, i will take a look when i have the time
-  // await knex.migrate.latest();
+  await knex.migrate.latest();
   await knex.raw(
     "truncate customers, orders, order_items, users, products, time_slots, slots, shops, sections cascade;"
   );

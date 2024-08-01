b11a2824d4705f706dc9ac0cb43776ad1aac355c
Dimitris Klouvas <dimitris.klouvas@gmail.com>
2024-08-02 01:23:06 +0300

    chore(api): Drop unused publicKey exposed in /discovery api

--- api/src/discovery/router.ts
+++ api/src/discovery/router.ts
@@ -2,12 +2,11 @@ import type { Context } from "koa";
 
 import { DiscoveryApiFactory } from "@dimkl/ajv-discovery-api";
 import Router from "koa-router";
-import { getPublicKey } from "../shared/getPublicKey";
 
 export const router = new Router();
 
 const discoveryApi = DiscoveryApiFactory.getInstance();
 
 router.get("/discovery", (ctx: Context) => {
-  ctx.body = { schemas: discoveryApi.getSchemas(), publicKey: getPublicKey() };
+  ctx.body = { schemas: discoveryApi.getSchemas() };
 });
--- api/src/shared/getPublicKey.ts
+++
@@ -1,3 +0,0 @@
-import type { Secret } from "jsonwebtoken";
-
-export const getPublicKey = () => process.env.JWT_PUBLIC_KEY as Secret;

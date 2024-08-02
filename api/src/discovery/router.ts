46df42da472dff578bd8baefa3ae936dc7e6d0fe
Dimitris Klouvas <dimitris.klouvas@gmail.com>
2024-08-02 03:13:45 +0300

    feat(api): Expose OpenAPI html page from API

--- api/src/discovery/router.ts
+++ api/src/discovery/router.ts
@@ -3,10 +3,19 @@ import type { Context } from "koa";
 import { DiscoveryApiFactory } from "@dimkl/ajv-discovery-api";
 import Router from "koa-router";
 
+import { readFileSync } from "fs";
+
 export const router = new Router();
 
 const discoveryApi = DiscoveryApiFactory.getInstance();
 
 router.get("/discovery", (ctx: Context) => {
   ctx.body = { schemas: discoveryApi.getSchemas() };
+});
+
+// TODO(dimkl): expose it from redocly or using koa-static instead of reading the file
+const openAPIPreviewHtml = readFileSync(__dirname + "/../../dist/preview.html").toString();
+router.get("/openapi", (ctx: Context) => {
+  ctx.type = "text/html";
+  ctx.body = openAPIPreviewHtml;
 });

1cccb44a2dc3e20a04f5b763ffe63ffe7b060893
Dimitris Klouvas <dimitris.klouvas@gmail.com>
2024-09-02 09:55:49 +0300

    feat(api): Support product ingredient addition with ingredients object list

--- api/src/products/controllers/products/addIngredient.ts
+++ api/src/products/controllers/products/addIngredient.ts
@@ -1,13 +1,30 @@
 import type { Context } from "koa";
 
 import schema from "../../schemas/productIngredient.create.json";
-import { ProductIngredient, Product } from "../../models";
+import { ProductIngredient, Ingredient, Product } from "../../models";
 
 const handler = async (ctx: Context) => {
   // @ts-expect-error validatedData are added as part of the request validation
-  const data = ctx.request.validatedData;
+  const { ingredients, shop_id, ...data } = ctx.request.validatedData;
 
-  await ProductIngredient.query().insert(data);
+  let prdIngr = [data];
+  if (ingredients.length) {
+    // TODO: validate that shop id exists in request
+
+    ingredients.forEach((ingr) => {
+      ingr.shop_id = shop_id;
+    });
+
+    const createdIngredients = await Ingredient.query()
+      .modify("publicInsertColumns")
+      .insert(ingredients as Ingredient[]);
+
+    prdIngr = createdIngredients.map((ingr) => {
+      return { ...data, ingredient_id: ingr.id };
+    });
+  }
+
+  await ProductIngredient.query().insert(prdIngr);
 
   ctx.body = await Product.findWithIngredients(data.product_id);
 };
